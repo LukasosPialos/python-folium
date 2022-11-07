c777a89dd91a7ff030c67ac085ec12799eaf8c91
Lukas <lukaspiala69@gmail.com>
2022-11-07 20:22:44 +0100

    oprava

--- main.py
+++ main.py
@@ -2,30 +2,28 @@ import pandas
 import folium
 import csv
 
-kraje_slovenske = f"http://download.freemap.sk/AdminLevel/kraje.json"
+#kraje_slovenske = f"http://download.freemap.sk/AdminLevel/kraje.json"
 
 
 
 
 df = pandas.read_csv('emisie.csv')
-INDICATOR = 'PROD_TONY'
+INDICATOR = 'Kraj'
 data = df[df['Kraj'] == INDICATOR]
 max_year = data['Rok'].max()
 data = data[data['Rok'] == max_year]
 
 map_data = data[['Kraj', 'Hodnota']]
-map_data.head()
-print(map_data)
 
 m = folium.Map(location=[49.130606, 18.681453], zoom_start=7)
 folium.Choropleth(
-    geo_data=kraje_slovenske,
+    geo_data='kraje.json',
     data=map_data,
     columns=['Kraj', 'Hodnota'],
-    key_on='features.properties',
+    key_on='properties.REF',
     fill_color='YlGnBu',
     fill_opacity=0.7,
     line_opacity=0.2,
-    legend_name='Produkcia emisii v tonach'
+    legend_name='Produkcia emisii v tonach za rok 2019'
 ).add_to(m)
-ms = m.save('cum.html')
+ms = m.save('mapa.html')
